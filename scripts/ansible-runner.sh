cb03f4843cf711169777bd48dde4c92b78b3cd42
Joel Castillo <jocastillo@records.nyc.gov>
2021-05-21 14:52:00 -0400

    Use pipenv to run ansible

--- scripts/ansible-runner.sh
+++ scripts/ansible-runner.sh
@@ -8,5 +8,5 @@ database_user_password=$5
 pip install pipenv
 pipenv install
 
-ansible-playbook --extra-vars "{\"database_name\": $1, \"server_fqdn\": $2, \"administrator_user\": $3, \"administrator_password\": $4, \"database_user_password\": $5
+pipenv run ansible-playbook --extra-vars "{\"database_name\": $1, \"server_fqdn\": $2, \"administrator_user\": $3, \"administrator_password\": $4, \"database_user_password\": $5
 }" --connection=local -i 127.0.0.1 ../playbook-ansible/main.yml
